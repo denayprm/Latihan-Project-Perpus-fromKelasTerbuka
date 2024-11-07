91abd11c6e17d02598c455211ecf120f4ce00fc1
denayprm <152400121+denayprm@users.noreply.github.com>
2024-11-07 11:55:05 +0700

    Init Database dan Read_Kelas Terbuka

---
+++ .vscode/CRUD/Database.py
@@ -0,0 +1,17 @@
+from . import Operasi
+
+DB_NAME = "data.txt"
+TEMPLATE = {
+    "pk":"XXXXXX",
+    "date_add":"yyyy-mm-dd",
+    "judul":255*" ",
+    "penulis":255*" ",
+    "tahun":"yyyy"
+}
+def init_console():
+    try:
+        with open(DB_NAME, "r") as file:
+            print("Database tersedia, init done!")
+    except:
+        print("Databes tidak ditemukan, silahkan membuat database baru")
+        Operasi.create_first_data()
