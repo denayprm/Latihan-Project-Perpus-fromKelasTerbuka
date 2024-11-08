34e4d594720218898c5eb43b2d9ab1bbc6a134ef
denayprm <152400121+denayprm@users.noreply.github.com>
2024-11-08 23:12:34 +0700

    Delete_Kelas Terbuka

--- CRUD/Operasi.py
+++ CRUD/Operasi.py
@@ -5,31 +5,12 @@ import time
 import os
 
 def delete(no_buku):
-    try:
-        with open(Database.DB_NAME,'r', encoding="utf-8") as file:
-            counter = 0
-            while(True):
-                content = file.readlines()
-                if len(content) == 0:
-                    break
-                elif counter == no_buku - 1:
-                    pass
-                else:
-                    with open("data_temp.txt", 'a', encoding="utf-8") as temp_file:
-                        temp_file.write(content)
-                counter += 1
-    except:
-        print("Database Error")
-
-    os.rename("data_temp.txt", Database.DB_NAME)
-
-def delete(no_buku):
+    print(Database.DB_NAME)
     try:
         with open(Database.DB_NAME,'r') as file:
             counter = 0
-
             while(True):
-                content = file.readline()
+                content = file.readlines()
                 if len(content) == 0:
                     break
                 elif counter == no_buku - 1:
